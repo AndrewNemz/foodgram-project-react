a90d43d9f11f2e8be113c8428a65c73a0646013d
Andrew Nemzorov <nemzorovandrew@mail.ru>
2023-05-27 21:34:20 +0300

    fix csv command

--- backend/recipes/management/commands/fill_db.py
+++ backend/recipes/management/commands/fill_db.py
@@ -1,26 +1,27 @@
 import csv
 
-from django.core.management.base import BaseCommand
-
+from django.core.management import BaseCommand
 from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = 'Импорт данных из csv в модель Ingredient'
-
-    def add_arguments(self, parser):
-        parser.add_argument('--path', type=str, help='Путь к файлу')
-
+    """
+    Наполнение базы данных данными из ingredients.csv.
+    Команда - pyhton manage.py fill_db.
+    """
     def handle(self, *args, **options):
-        print('Загрузка данными базы данных началась.')
-        file_path = 'data/ingredients.csv'
-        with open(file_path, 'r', encoding='utf-8') as csv_file:
-            reader = csv.reader(csv_file)
 
-            for row in reader:
-                obj, created = Ingredient.objects.get_or_create(
-                    name=row[0],
-                    measure_unit=row[1],
+        file_path = 'data/ingredients.csv'
+        print('Загрузка началась')
+
+        with open(file_path, 'r', encoding='utf-8') as csv_file:
+            ingredients = csv.reader(csv_file)
+
+            for row in ingredients:
+                name, measure_unit = row
+                Ingredient.objects.get_or_create(
+                    name=name,
+                    measure_unit=measure_unit
                 )
 
-        print('Загрузка завершена.')
+        print('Загрузка успешно завершена')
